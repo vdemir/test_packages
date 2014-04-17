6461f0a5b5e2dc2050345301e866621993a333f7
Vedat Demir <v.demir@mail.com>
2014-04-17 23:50:43 +0300

    first release

--- gendesk/actions.py
+++ gendesk/actions.py
@@ -7,15 +7,12 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 
-# if pisi can't find source directory, see /var/pisi/gendesk/work/ and:
-# WorkDir="gendesk-"+ get.srcVERSION() +"/sub_project_dir/"
-
 def build():
 
 shelltools.cd("%s/" % get.workDIR())  
 shelltools.system("go build")
 
-#def install():
-#    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
 # By PiSiDo 2.0.0
