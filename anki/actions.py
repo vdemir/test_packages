032d32641c56fb7e71d14ccd6af038b03bc938bf
Vedat Demir <v.demir@mail.com>
2014-04-19 14:55:12 +0300

    first release

--- anki/actions.py
+++ anki/actions.py
@@ -3,10 +3,9 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import get
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 def install():
 	
@@ -17,4 +16,6 @@ def install():
     pisitools.insinto("/usr/share/pixmaps/", "anki.xpm")
     pisitools.insinto("/usr/share/applications/", "anki.desktop")
     pisitools.insinto("/usr/share/anki/", "thirdparty/send2trash")
+    for lang in ["de", "es", "fr", "it", "nl", "pl", "tr"]:
+	     pisitools.insinto("/usr/share/locale/%s/LC_MESSAGES/" %lang, "locale/%s/LC_MESSAGES/anki.mo" %lang, lang)
     pisitools.dodoc("README", "LICENSE")
