032e0d58c831f31e1aa543990f1ee2300231129b
vedat <vedat@pisi_linux1.0>
2014-04-26 17:27:16 +0300

    lxde-qt

---
+++ lx-qt/lxqt-powermanagement/actions.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+# if pisi can't find source directory, see /var/pisi/liblxqt/work/ and:
+# WorkDir="liblxqt-"+ get.srcVERSION() +"/sub_project_dir/"
+
+#QT_INSTALL_PREFIX=/usr/include/Qt"
+
+def setup():
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=/usr -DINCLUDE_INSTALL_DIR=/usr/include/Qt -DQT_INCLUDE_DIR=/usr/include/qt -DCMAKE_INSTALL_LIBDIR=/usr/lib")
+#    pisitools.dosed("CMakeCache.txt", "qt4", "qt")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+# Take a look at the source folder for these file as documentation.
+#    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "README")
+
+# If there is no install rule for a runnable binary, you can 
+# install it to binary directory.
+#    pisitools.dobin("liblxqt")
+
+# You can use these as variables, they will replace GUI values before build.
+# Package Name : liblxqt
+# Version : 1.0
+# Summary : Common base library for LXQt components.
+
+# For more information, you can look at the Actions API
+# from the Help menu and toolbar.
+
+# By PiSiDo 2.0.0
