d1efd1b973a232da156dd69f70061630d634c632
Vedat Demir <v.demir@mail.com>
2014-04-30 23:28:14 +0300

    libfm: fix action.py

--- lx-qt/libfm/actions.py
+++ lx-qt/libfm/actions.py
@@ -6,20 +6,19 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import get
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import libtools
-from pisi.actionsapi import pisitools
+
+
  
 def setup():
     
     autotools.configure("--prefix=/usr -enable-shared --disable-static --without-gtk  --disable-gtk-doc \
-                         --enable-udisks --enable-actions --disable-demo --sysconfdir=/etc \
-			 GIO_LIBS='-L/usr/lib -lgio-2.0 -lgobject-2.0 -lglib-2.0 -ldbusmenu-qt -ldbusmenu-glib' \
+                         --enable-udisks --enable-actions --sysconfdir=/etc \
+			 GIO_LIBS='-L/usr/lib -lgio-2.0 -lgobject-2.0 -lglib-2.0' \
                          GIO_CFLAGS='-I/usr/include/glib-2.0/ -I/usr/include/gio-unix-2.0 -I/usr/lib/glib-2.0/include' \
-			 DBUS_LIBS='-L/usr/lib -ldbusmenu-glib -ldbusmenu-qt -ldbus-c++-1 -ldbus-glib-1 -ldbus-1' \
-		         DBUS_CFLAGS='-I/usr/include/dbus-1.0 -I/usr/include/libdbusmenu-glib-0.4 -I/usr/include/dbusmenu-qt' \
+			 DBUS_LIBS='-L/usr/lib -ldbus-glib-1 -ldbus-1' \
+		         DBUS_CFLAGS='-I/usr/include/dbus-1.0' \
                          MENU_CACHE_LIBS='-L/usr/lib -lmenu-cache' \
-			 MENU_CACHE_CFLAGS=-I/usr/include/menu-cache/ ")
+			 MENU_CACHE_CFLAGS=-I/usr/include/menu-cache/")
  	                                  
 def build():
      autotools.make()
