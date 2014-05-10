4909b459a85afa888cefd75587a97874adf7740a
vedat <vedat@pisi_linux1.0>
2014-05-10 12:27:47 +0300

    some fixes

--- LXQT/LXQT-Desktop/qtermwidget/actions.py
+++ LXQT/LXQT-Desktop/qtermwidget/actions.py
@@ -8,11 +8,11 @@ from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
 def setup():
-    cmaketools.configure("-DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=/usr/lib -DCMAKE_INCLUDE_DIR=/usr/include/qtermwidget/")
-#-DCMAKE_BUILD_TYPE=RelWithDebInfo-DCMAKE_BUILD_TYPE=RelWithDebInfo-DCMAKE_INSTALL_LIBDIR=/usr/lib
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=/usr/lib")
+
 def build():
     cmaketools.make()
 
 def install():
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("AUTHORS", "COPYING")
+    pisitools.dodoc("AUTHORS", "COPYING", "README")
