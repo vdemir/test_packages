58f61f1e38371c025f499013397b60629ae80a63
vedat <vedat@pisi_linux1.0>
2014-05-13 21:53:19 +0300

    some fixes

--- LXQT/Base libraries/menu-cache/actions.py
+++ LXQT/Base libraries/menu-cache/actions.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
+
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
 
 def setup():
-    autotools.configure("--prefix=/usr --disable-gtk-doc --disable-static")
-
+    shelltools.system("./autogen.sh --enable-shared --disable-static \
+				    --without-gtk --disable-gtk-doc \
+				    --enable-udisks --disable-actions \
+                                    --disable-demo --disable-dependency-tracking \
+                                    --prefix=/usr --libdir=/usr/lib --sysconfdir=/etc \
+                                    --enable-fast-install=autogen")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("AUTHORS", "COPYING", "README")
+    pisitools.dodoc("AUTHORS", "COPYING")
